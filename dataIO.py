369d80690d2fa38e2f3a393286ca5be68569caa6
Matt <90409146+Matt-Crane@users.noreply.github.com>
2024-01-11 16:26:51 +0000

    Plugged in to Heroku DB

--- dataIO.py
+++ dataIO.py
@@ -7,21 +7,21 @@ import sqlalchemy
 
 def connect():
     # need to swap password for local var
-    cnxn = sqlalchemy.create_engine('mysql+pymysql://bq21582:password_password@127.0.0.1:3306/ukllc')
+    cnxn = sqlalchemy.create_engine('mysql+pymysql://***REMOVED***')
     return(cnxn)
 
 
 def load_blocks(cnxn):
-    return pd.read_sql("SELECT * from ukllc.dataset", cnxn)
+    return pd.read_sql("SELECT * from dataset", cnxn)
 
 def load_sources(cnxn):
-    return pd.read_sql("SELECT * from ukllc.datasource", cnxn)
+    return pd.read_sql("SELECT * from datasource", cnxn)
 
 
 def load_study_request(cnxn):
     '''
     '''
-    sheet_df = pd.read_sql("SELECT * from ukllc.drf_lps", cnxn)
+    sheet_df = pd.read_sql("SELECT * from drf_lps", cnxn)
     #sheet_df = pd.read_excel(os.path.join("assets", "Data Request Form.xlsx"), sheet_name="Study data requested",skiprows=5, usecols = "D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R")
     
     return sheet_df
@@ -36,7 +36,7 @@ def load_linked_request(cnxn):
     #sheet_df = pd.read_excel(os.path.join("assets", "Data Request Form.xlsx"), sheet_name="Linked data requested",skiprows=5, usecols = "A,B,C,D,E,F,G,H")
     #sheet_df = sheet_df.rename(columns = {"Data Block Name":"Block Name", "Data Block Description":"Block Description", "Time Period†":"Timepoint: Data Collected", "'Health Domain Groupings (i.e. covid infection, asthma, smoking, etc.)":"Keywords"})
     #sheet_df["Source"] = "NHSD"
-    sheet_df = pd.read_sql("SELECT * from ukllc.drf_nhs", cnxn)
+    sheet_df = pd.read_sql("SELECT * from drf_nhs", cnxn)
 
     return sheet_df
 
@@ -46,7 +46,7 @@ def load_study_info_and_links(cnxn):
     '''
     #TODO Convert to database
     #sheet_df = pd.read_excel(os.path.join("assets", "Data Request Form.xlsx"), sheet_name="Study info & links", skiprows=1, usecols = "B,C,D,E,F,G,H,I,J" )
-    sheet_df = pd.read_sql("SELECT * from ukllc.study_info", cnxn)
+    sheet_df = pd.read_sql("SELECT * from study_info", cnxn)
     return sheet_df
 
 def load_study_metadata(cnxn, table_id):
@@ -58,7 +58,7 @@ def load_study_metadata(cnxn, table_id):
     # TODO change to joined metadata file (requires preprep, splitting all into proper folders)
     try:
         #values_df = pd.read_csv(os.path.join("metadata",str(study.upper()),table+".csv"))
-        values_df = pd.read_sql("SELECT * from ukllc.{}".format(study.lower()+"_"+table.lower()), cnxn) 
+        values_df = pd.read_sql("SELECT * from {}".format(study.lower()+"_"+table.lower()), cnxn) 
     except FileNotFoundError:
         print("Couldn't find file {}. Skipping (shouldn't be a problem when we have a db...".format(str(study.upper())+table+".csv"))
         return None
